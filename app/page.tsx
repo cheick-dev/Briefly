4558db61c5f2ce187ac6d8a327570d0888c8040c
cheick <cheickdev1010@gmail.com>
2025-01-27 11:59:21 +0000

    Added some animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,42 @@
 import Link from "next/link";
 import { Book } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { TextLoop } from "@/components/ui/text-loop";
+import { Magnetic } from "@/components/ui/magnetic";
 
 export default function Home() {
 	return (
 		<>
 			<div className="flex min-h-[calc(100vh-4rem)] container mx-auto flex-col items-center justify-center px-4 text-center">
-				<div className="max-w-3xl space-y-6">
+				<div className="max-w-4xl space-y-6">
 					<div className="flex justify-center">
 						<Book className="h-16 w-16" />
 					</div>
-					<h1 className="text-4xl font-bold sm:text-6xl">
-						Transform Content into
-						<span className="text-primary"> Concise Summaries</span>
+					<h1 className="text-3xl font-bold sm:text-6xl">
+						Transformez vos contenus en{" "}
+						<span className="text-primary">
+							résumés instantanés.
+						</span>
 					</h1>
 					<p className="text-lg text-muted-foreground sm:text-xl">
-						Upload articles, PDFs, or any content and get AI-powered
-						summaries tailored for tech enthusiasts. Save time while
-						staying informed.
+						<TextLoop className="text-primary text-2xl">
+							<span>Articles ?</span>
+							<span>Documents(PDFs, Word, Excel et Txt) ?</span>
+							<span>Liens YouTube ?</span>
+						</TextLoop>
+						<br />
+						Briefly résume ces contenus en un seul clic grâce à
+						l'IA.
 					</p>
 					<div className="flex justify-center gap-4">
-						<Button asChild size="lg">
-							<Link href="/dashboard">Get Started</Link>
-						</Button>
-						<Button variant="outline" size="lg" asChild>
-							<Link href="#features">Learn More</Link>
-						</Button>
+						<Magnetic>
+							<Button asChild size="lg">
+								<Link href="/dashboard">Allons-y !</Link>
+							</Button>
+						</Magnetic>
 					</div>
 				</div>
 			</div>
-			{/* <FileUploader /> */}
-			{/* <div className="min-h-screen bg-background text-foreground">
-				<Header />
-				<main>
-					<HeroSection />
-					<KeyFeatures />
-					<HowItWorks />
-					<Showcase />
-					<Testimonials />
-					<CallToAction />
-				</main>
-				<Footer />
-			</div> */}
 		</>
 	);
 }
